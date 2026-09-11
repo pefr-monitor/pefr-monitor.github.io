5c962757db5007aad38253d9f1da612267301140
kyung-eun0418 <kyeong85@naver.com>
2026-09-11 12:30:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="pefr-v2-2-autotime";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
+const CACHE="pefr-v2-3-illustration";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./pefr-measurement-guide.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
